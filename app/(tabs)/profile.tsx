cf5e43b29bbda2caa9bae86c2f0b326ecd7e55c8
asifalin <username@users.noreply.github.com>
2025-03-06 14:23:26 +0500

    tbas complte

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+      <Text style={{backgroundColor:'red'}}>my name is laiba ali from gilgit</Text>
+      <Text style={{backgroundColor:'blue',color:'white',fontSize:30}}>my name is asif ali front end  developer</Text>
+    </View>
+  )
+}
+
+export default Profile
